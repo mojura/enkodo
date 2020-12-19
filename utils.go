6047da0a2527ea58d2f40a155c8cc9aa8fb7d8cf
Josh Montoya <josh@montoya.io>
2020-12-19 10:28:51 -0800

    implement Marshal, MarshalAppend, Unmarshal

--- utils.go
+++ utils.go
@@ -134,3 +134,21 @@ func (b *BinaryReader) Int64(bs []byte) (v int64, err error) {
 func getStringBytes(str string) []byte {
 	return *((*[]byte)(unsafe.Pointer(&str)))
 }
+
+// Marshal will encode a value
+func Marshal(v Encodee) (bs []byte) {
+	return MarshalAppend(v, nil)
+}
+
+// MarshalAppend will encode a value to a provided slice
+func MarshalAppend(v Encodee, buffer []byte) (bs []byte) {
+	enc := newEncoder(buffer)
+	enc.Encode(v)
+	return enc.bs
+}
+
+// Unmarshal will decode a value
+func Unmarshal(bs []byte, v Decodee) (err error) {
+	dec := newDecoder(bs)
+	return dec.Decode(v)
+}
