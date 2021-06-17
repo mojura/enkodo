266c473834270be3d76aaaecc59658ad6eb75b06
Josh Montoya <josh@montoya.io>
2021-06-17 15:43:05 -0700

    implement schema interface and basicSchema type

---
+++ schema.go
@@ -0,0 +1,76 @@
+package enkodo
+
+import (
+	"fmt"
+	"reflect"
+)
+
+var basicSchemas = map[reflect.Kind]Schema{
+	reflect.String:  &basicSchema{efn: genericEncodeString, dfn: genericDecodeString},
+	reflect.Bool:    &basicSchema{efn: genericEncodeBool, dfn: genericDecodeBool},
+	reflect.Int:     &basicSchema{efn: genericEncodeInt, dfn: genericDecodeInt},
+	reflect.Int8:    &basicSchema{efn: genericEncodeInt8, dfn: genericDecodeInt8},
+	reflect.Int16:   &basicSchema{efn: genericEncodeInt16, dfn: genericDecodeInt16},
+	reflect.Int32:   &basicSchema{efn: genericEncodeInt32, dfn: genericDecodeInt32},
+	reflect.Int64:   &basicSchema{efn: genericEncodeInt64, dfn: genericDecodeInt64},
+	reflect.Uint:    &basicSchema{efn: genericEncodeUint, dfn: genericDecodeUint},
+	reflect.Uint8:   &basicSchema{efn: genericEncodeUint8, dfn: genericDecodeUint8},
+	reflect.Uint16:  &basicSchema{efn: genericEncodeUint16, dfn: genericDecodeUint16},
+	reflect.Uint32:  &basicSchema{efn: genericEncodeUint32, dfn: genericDecodeUint32},
+	reflect.Uint64:  &basicSchema{efn: genericEncodeUint64, dfn: genericDecodeUint64},
+	reflect.Float32: &basicSchema{efn: genericEncodeFloat32, dfn: genericDecodeFloat32},
+	reflect.Float64: &basicSchema{efn: genericEncodeFloat64, dfn: genericDecodeFloat64},
+}
+
+// MakeSchema will initialize a new Schema
+func MakeSchema(val interface{}) (s Schema, err error) {
+	typ := reflect.TypeOf(val)
+	return c.GetOrCreate(typ)
+}
+
+type Schema interface {
+	MarshalEnkodo(enc *Encoder, val interface{}) error
+	UnmarshalEnkodo(dec *Decoder, field *reflect.Value) error
+}
+
+func makeSchema(rtype reflect.Type) (s Schema, err error) {
+	var ok bool
+	kind := rtype.Kind()
+	if s, ok = basicSchemas[kind]; ok {
+		return
+	}
+
+	switch kind {
+	case reflect.Struct:
+		return newStructSchema(rtype)
+	case reflect.Slice:
+		return newSliceSchema(rtype)
+	case reflect.Map:
+		return newMapSchema(rtype)
+
+	// TODO: Implement this
+	//case reflect.Array:
+	//	return
+
+	default:
+		err = fmt.Errorf("type of %s is not supported", rtype.Kind())
+		return
+	}
+}
+
+type encoderFn func(*Encoder, interface{}) error
+
+type decoderFn func(*Decoder, *reflect.Value) error
+
+type basicSchema struct {
+	efn encoderFn
+	dfn decoderFn
+}
+
+func (s *basicSchema) MarshalEnkodo(enc *Encoder, val interface{}) (err error) {
+	return s.efn(enc, val)
+}
+
+func (s *basicSchema) UnmarshalEnkodo(dec *Decoder, val *reflect.Value) (err error) {
+	return s.dfn(dec, val)
+}
---
+++ schema_test.go
@@ -0,0 +1,111 @@
+package enkodo
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type schemaTestLarge struct {
+	A []interface{}
+	B map[string]interface{}
+
+	Medium schemaTestMedium
+}
+
+type schemaTestMedium struct {
+	A []string
+	B []uint64
+	C []bool
+
+	D map[string]string
+	E map[string]uint64
+	F map[string]bool
+
+	Small schemaTestSmall
+}
+
+type schemaTestSmall struct {
+	A string
+	B uint64
+	C bool
+}
+
+func TestMakeSchema(t *testing.T) {
+	var val schemaTestSmall
+	val.A = "foo"
+	val.B = 63
+	val.C = true
+
+	var err error
+	if _, err = MakeSchema(val); err != nil {
+		t.Fatal(err)
+	}
+}
+func TestSchema_Marshal_Unmarshal(t *testing.T) {
+	val := schemaTestLarge{
+		A: []interface{}{
+			"foo",
+			"bar",
+			"baz",
+			1337,
+		},
+		B: map[string]interface{}{
+			"foo": "1",
+			"bar": true,
+			"baz": 1337,
+		},
+		Medium: schemaTestMedium{
+			A: []string{"foo", "bar", "baz"},
+			B: []uint64{63, 126, 811},
+			C: []bool{true, false, true},
+			D: map[string]string{
+				"foo": "1",
+				"bar": "2",
+				"baz": "3",
+			},
+			E: map[string]uint64{
+				"foo": 1,
+				"bar": 2,
+				"baz": 3,
+			},
+			F: map[string]bool{
+				"foo": true,
+				"bar": false,
+				"baz": true,
+			},
+			Small: schemaTestSmall{
+				A: "bar",
+				B: 1337,
+				C: true,
+			},
+		},
+	}
+
+	var (
+		s   Schema
+		err error
+	)
+
+	if s, err = MakeSchema(val); err != nil {
+		t.Fatal(err)
+	}
+
+	buf := bytes.NewBuffer(nil)
+	enc := newEncoder(buf)
+
+	if err = s.MarshalEnkodo(enc, val); err != nil {
+		t.Fatal(err)
+	}
+
+	dec := newDecoder(buf)
+
+	var ref schemaTestLarge
+	rval := reflect.ValueOf(&ref)
+	if err = s.UnmarshalEnkodo(dec, &rval); err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n\n%+v\n", val.Medium, ref)
+}
