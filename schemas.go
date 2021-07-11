5a57b03daa94b816baa70c5454e66a79bc1597c4
Josh Montoya <josh@montoya.io>
2021-07-11 10:20:01 -0700

    implement newBasicSchema

--- schemas.go
+++ schemas.go
@@ -2,7 +2,16 @@ package enkodo
 
 import "reflect"
 
+func newBasicSchema(val interface{}, efn encoderFn, dfn decoderFn) *basicSchema {
+	var b basicSchema
+	b.typ = reflect.TypeOf(val)
+	b.efn = efn
+	b.dfn = dfn
+	return &b
+}
+
 type basicSchema struct {
+	typ reflect.Type
 	efn encoderFn
 	dfn decoderFn
 }
